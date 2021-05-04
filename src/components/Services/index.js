41cf34e74bfa853733297140bf3887342424ff68
Hey Jason <jyoon1989@hotmail.com>
2021-05-04 13:32:00 -0400

    First commit - dolla

---
+++ src/components/Services/index.js
@@ -0,0 +1,45 @@
+import {
+  ServicesContainer,
+  ServicesWrapper,
+  ServicesH1,
+  ServicesH2,
+  ServicesCard,
+  ServicesP,
+  ServicesIcon,
+} from './ServicesElements';
+import Icon1 from '../../images/secure_data.svg';
+import Icon2 from '../../images/data_report.svg';
+import Icon3 from '../../images/Savings.svg';
+
+const Services = () => {
+  return (
+    <ServicesContainer id='services'>
+      <ServicesH1>Our Services</ServicesH1>
+      <ServicesWrapper>
+        <ServicesCard>
+          <ServicesIcon src={Icon1} />
+          <ServicesH2>Reduce Expenses</ServicesH2>
+          <ServicesP>
+            We help reduce your fees and increase your overall revenue.
+          </ServicesP>
+        </ServicesCard>
+        <ServicesCard>
+          <ServicesIcon src={Icon2} />
+          <ServicesH2>Virtual Offices</ServicesH2>
+          <ServicesP>
+            You can access our platform online anywhere in the world.
+          </ServicesP>
+        </ServicesCard>
+        <ServicesCard>
+          <ServicesIcon src={Icon3} />
+          <ServicesH2>Premium Benefits</ServicesH2>
+          <ServicesP>
+            Unlock our special membership card that returns 5% cash back.
+          </ServicesP>
+        </ServicesCard>
+      </ServicesWrapper>
+    </ServicesContainer>
+  );
+};
+
+export default Services;
